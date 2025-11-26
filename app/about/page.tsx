2b70096f5558bba3309e18faa892e158aaca0028
omerf130 <ofaclieru@gmail.com>
2025-11-26 11:56:44 +0200

    css

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import Image from "next/image";
+import classes from "./page.module.scss";
+import Link from "next/link";
+import { CONSTS } from "../_consts/consts";
 
 const page = () => {
+  const {HEADER, CONTENT1, CONTENT2, BUTTON} = CONSTS.ABOUT;
+
   return (
-    <div>
-      about
+
+    <div className={classes.root}>
+      <div className={classes.header}>
+        <div className={classes.logoWrapper}>
+          <Image
+            className={classes.logo}
+            src="/logo/logo1.png"
+            alt="Logo"
+            width={150}
+            height={150}
+            unoptimized
+          />
+        </div>
+        <h1>{HEADER}</h1>
+      </div>
+      <div className={classes.content}>
+        <h2>{CONTENT1}</h2>
+        <h2>{CONTENT2}</h2>
+      </div>
+      <div className={classes.actions}>
+        <Link className="button-primary" href="/contact">
+          {BUTTON}
+        </Link>
+      </div>
     </div>
   )
 }
---
+++ app/dance/page.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import classes from './page.module.scss'
+import Image from "next/image";
+
+const page = () => {
+    return (
+        <main className={classes.root}>
+            <section className={classes.danceContent}>
+                <div className={classes.headers}>
+                    <h1>לרקוד איתנו</h1>
+                    <h2>מסיבות שקיעה בימי שבת - פוגשים את הקהילה עם מוזיקה סוחפת (לצד סרטון)
+                        ההרשמה באיזון מגדרי בשביל להבטיח מקסימום הנאה!</h2>
+                    <h2>ימי שבת – 18:00 עד 22:00 </h2>
+                </div>
+                <div className={classes.links}>
+                    <Link className="navbar-link" href="/contact">
+                        &lt;&lt;&lt; להרשמה
+                    </Link>
+                </div>
+                <div className={classes.bottom}>
+                    <h1>ממלאים פרטים אישיים
+                        ומשריינים מקום בלו"ז</h1>
+                </div>
+            </section>
+            <div className={classes.imageWrapper}>
+                <Image
+                className={classes.image}
+                src="/pics/pic3.jpg"
+                alt="Logo"
+                width={150}
+                height={150}
+                unoptimized
+            />
+            </div>
+        </main>
+    )
+}
+
+export default page
